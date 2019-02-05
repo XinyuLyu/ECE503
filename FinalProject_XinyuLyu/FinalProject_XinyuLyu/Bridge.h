57331f7792df0be169f6a46f53c9034c291fa11f
XinyuLyu <xl422@scarletmail.rutgers.edu>
2019-02-05 15:44:03 -0500

    push

--- FinalProject_XinyuLyu/FinalProject_XinyuLyu/Bridge.h
+++
@@ -1,7 +0,0 @@
-#pragma once                          
-class Account; 	                           
-class Bridge  {	             //abstract part in bridge design pattern    
-public:
-	virtual void Implement(Account *a) {};
-};
-
--- FinalProject_XinyuLyu/FinalProject_XinyuLyu/print_Title.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class print                          // a design pattern: adapter class to fufill different requests for print from differet client
-{
-public:
-	virtual void print_Title()=0;
-};
-
